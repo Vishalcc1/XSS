0d449ed0603b842e4b834ece7d6185e3c5763a45
Kethan <mskethan@gmail.com>
2021-10-29 20:16:12 +0530

    get method and indentation changes

--- XSS/DOM/domfinder.py
+++ XSS/DOM/domfinder.py
@@ -1,12 +1,14 @@
 import requests
 import concurrent.futures
 from lib import EstableSession
-args1=[]
-sourcelist,sinklist=[],[]
+
+args1 = []
+sourcelist, sinklist = [], []
+
 
 def sourceadder():
     try:
-        file=open("DOM/source.txt")
+        file = open("DOM/source.txt")
         """Fetch a source file and add all vulnerable keyword into soup"""
         for line in file:
             sourcelist.append(line.strip('\n'))
@@ -15,9 +17,10 @@ def sourceadder():
     except:
         print("File not Found...")
 
+
 def sinkadder():
     try:
-        file=open("DOM/sink.txt")
+        file = open("DOM/sink.txt")
         """Fetch a sink file and add all vulnerable keyword into soup"""
         for line in file:
             sinklist.append(line.strip('\n'))
@@ -26,20 +29,21 @@ def sinkadder():
     except:
         print("File not found...")
 
+
 def checker(url):
     url, soup = url
     req = requests.get(url)
     for source in sourcelist:
-        if(source != '' and source in req.text):
-            print("DOM Base vulnerability :- [",source,"] find in ",url)
+        if source != '' and source in req.text:
+            print("DOM Base vulnerability :- [", source, "] find in ", url)
     for sink in sinklist:
-        if (sink != '' and sink in req.text):
+        if sink != '' and sink in req.text:
             print("DOM Base vulnerability :- [", sink, "] find in ", url)
 
 
-def finder(args,collection):
+def finder(args, collection):
     global args1
-    arg1=args
+    arg1 = args
     sourceadder()
     sinkadder()
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.thread) as executor:
